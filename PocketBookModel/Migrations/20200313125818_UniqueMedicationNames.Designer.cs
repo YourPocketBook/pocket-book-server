048ddd9b0dcb61686986cef7e6bd60e82b0b0963
Tony Richards <trichards57@outlook.com>
2020-03-13 12:59:16 +0000

    Added migration for unique medications.

--- PocketBookModel/Migrations/20200313125818_UniqueMedicationNames.Designer.cs
+++ PocketBookModel/Migrations/20200313125818_UniqueMedicationNames.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PocketBookModel;
 
 namespace PocketBookModel.Migrations
 {
     [DbContext(typeof(ApplicationDataContext))]
-    partial class ApplicationDataContextModelSnapshot : ModelSnapshot
+    [Migration("20200313125818_UniqueMedicationNames")]
+    partial class UniqueMedicationNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
