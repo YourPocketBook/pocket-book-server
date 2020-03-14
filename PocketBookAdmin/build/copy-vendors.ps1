4c43d8866481d93123d28716086c3ff760d21095
Tony Richards <trichards57@outlook.com>
2020-03-14 16:58:39 +0000

    Added jquery and UI scripts to the vendors area.

--- PocketBookAdmin/build/copy-vendors.ps1
+++ PocketBookAdmin/build/copy-vendors.ps1
@@ -12,3 +12,15 @@ cp node_modules/@coreui/icons/js/svgxuse.* wwwroot/vendors/coreui/icons/js/
 
 mkdir wwwroot/vendors/coreui/utils/
 cp node_modules/@coreui/utils/dist/coreui-utils.* wwwroot/vendors/coreui/utils/
+
+mkdir wwwroot/vendors/jquery/
+cp node_modules/jquery/dist/jquery.* wwwroot/vendors/jquery/
+
+mkdir wwwroot/vendors/jquery-validation/
+cp node_modules/jquery-validation/dist/jquery.validate.* wwwroot/vendors/jquery-validation/
+
+mkdir wwwroot/vendors/jquery-validation-unobtrusive/
+cp node_modules/jquery-validation-unobtrusive/dist/jquery.validate.* wwwroot/vendors/jquery-validation-unobtrusive/
+
+mkdir wwwroot/vendors/timeago/
+cp node_modules/timeago.js/dist/timeago.* wwwroot/vendors/timeago/
